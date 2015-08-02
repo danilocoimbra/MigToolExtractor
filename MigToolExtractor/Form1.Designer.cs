61d695dbd4b9b8fc9c01aac639adb810627dfd17
danilocoimbra <danilo_coimbra@yahoo.com.br>
2015-08-01 22:13:01 -0300

    2015-08-01

--- MigToolExtractor/Form1.Designer.cs
+++ MigToolExtractor/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace MigToolExtractor
+﻿namespace SimpleZIPxTractor
 {
     partial class Form1
     {
@@ -31,85 +31,187 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(183, 12);
+            this.button1.Location = new System.Drawing.Point(10, 82);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(415, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Extract it!";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 48);
+            this.textBox1.Location = new System.Drawing.Point(73, 20);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(430, 20);
+            this.textBox1.Size = new System.Drawing.Size(320, 20);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 74);
+            this.textBox2.Location = new System.Drawing.Point(73, 46);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(430, 20);
+            this.textBox2.Size = new System.Drawing.Size(320, 20);
             this.textBox2.TabIndex = 2;
             // 
-            // textBox3
+            // progressBar2
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 100);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(430, 20);
-            this.textBox3.TabIndex = 2;
+            this.progressBar2.Location = new System.Drawing.Point(6, 66);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(418, 23);
+            this.progressBar2.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = this.BackColor;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(6, 95);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(418, 20);
+            this.textBox4.TabIndex = 2;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.progressBar2);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 148);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(430, 123);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Status";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(202, 132);
+            this.label1.Location = new System.Drawing.Point(7, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
             // 
-            // progressBar2
+            // label2
             // 
-            this.progressBar2.Location = new System.Drawing.Point(12, 165);
-            this.progressBar2.Name = "progressBar2";
-            this.progressBar2.Size = new System.Drawing.Size(430, 23);
-            this.progressBar2.TabIndex = 4;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(395, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 20);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Source zip:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Destination:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(395, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 20);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(431, 119);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Files";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(454, 200);
-            this.Controls.Add(this.progressBar2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(455, 285);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Zip Extractor w/ Progress bar";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- MigToolExtractor/Form1.cs
+++ MigToolExtractor/Form1.cs
@@ -16,13 +16,13 @@ using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
 
 using System.IO;
 
-namespace MigToolExtractor
+
+namespace SimpleZIPxTractor
 {
     public partial class Form1 : Form
     {
-        private int _uptoFileCount;
-        private int _totalFileCount;
-
+        public string zipName = @"C:\temp\danilo.zip";
+        public string strFolderDestination = @"C:\TEMP\Danilo\FastZip";
 
         public Form1()
         {
@@ -34,83 +34,38 @@ namespace MigToolExtractor
 
         }
 
-        public void TestFastZipUnpack(string zipFileName, string targetDir)
-        {
-
-            _totalFileCount = 41;//FolderContentsCount(@"C:\Temp\Danilo\fastzip");
-
-            FastZipEvents events = new FastZipEvents();
-            events.ProcessFile = ProcessFileMethod;
-            FastZip fastZip = new FastZip(events);
-
-            string fileFilter = null;
-
-            // Will always overwrite if target filenames already exist
-            fastZip.ExtractZip(zipFileName, targetDir, fileFilter);
-        }
-
-        private void ProcessFileMethod(object sender, ScanEventArgs args)
-        {
-            _uptoFileCount++;
-            int percentCompleted = _uptoFileCount * 100 / _totalFileCount;
-            label1.Text = percentCompleted.ToString();
-            label1.Update();
-
-            // do something here with a progress bar
-            // file counts are easier as sizes take more work to calculate, and compression levels vary by file type
-
-            string fileName = args.Name;
-            // To terminate the process, set args.ContinueRunning = false
-            if (fileName == "stop on this file")
-                args.ContinueRunning = false;
-        }
-
-        private int FolderContentsCount(string path)
-        {
-            int result = Directory.GetFiles(path).Length;
-            string[] subFolders = Directory.GetDirectories(path);
-            foreach (string subFolder in subFolders)
-            {
-                result += FolderContentsCount(subFolder);
-            }
-            return result;
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            UnZip(@"C:\temp\danilo.zip", @"C:\TEMP\Danilo\FastZip");
-
-            //Thread myThread = new Thread(new ThreadStart(WorkThreadFunction));
-            //myThread.Start();
-            //myThread.Join();
-
+            if (File.Exists(textBox1.Text) && Directory.Exists(textBox2.Text))
+            {
+                Thread thread = new Thread(new ThreadStart(DoWork));
+                thread.Start();
+            }
+            else
+            {
+                MessageBox.Show("Check the path!");
+            }
         }
 
-        public void WorkThreadFunction()
+        public void DoWork()
         {
-            UnZip(@"C:\temp\danilo.zip", @"C:\TEMP\Danilo\FastZip");
+            button1.SafeInvoke(d => d.Enabled = false);
+            initprogressbar(progressBar2, ZipContentsCount(zipName) - 1);
+            UnZip(textBox1.Text, textBox2.Text);
+            label1.SafeInvoke(d => d.Text = string.Empty);
+            label2.SafeInvoke(d => d.Text = string.Empty);
+            textBox4.SafeInvoke(d => d.Text = string.Empty);
+            MessageBox.Show("Finished!");
+            button1.SafeInvoke(d => d.Enabled = true);
         }
 
         public void UnZip(string zipName, string FolderDestination)
         {
-            textBox1.Text = "Efetuando restore de " + FolderDestination + ". Aguarde!";
-            textBox1.Update();
-
+            label1.SafeInvoke(d => d.Text = "Restoring zip file to folder " + FolderDestination + ". Wait!");
             try
             {
-                int count = ZipContentsCount(zipName);
-
-                textBox2.Text = count.ToString();
-                textBox2.Update();
-
-                initprogressbar(progressBar2, count);
-
                 FastZipEvents zipevents = new FastZipEvents();
-                //zipevents.ProcessDirectory = new ProcessDirectoryHandler(ProcessDirectory);
                 zipevents.ProcessFile = new ProcessFileHandler(ProcessFile);
-
-                //FastZip.ConfirmOverwriteDelegate objDelegate;
-                //objDelegate = AddressOf NotifyClient
 
                 FastZip fz = new FastZip(zipevents);
                 fz.CreateEmptyDirectories = true;
@@ -120,7 +75,7 @@ namespace MigToolExtractor
 
         }
 
-        private int ZipContentsCount(string zipfile)
+        public int ZipContentsCount(string zipfile)
         {
             int count = 0;
             ZipInputStream strmZipInputStream = new ZipInputStream(File.OpenRead(zipfile));
@@ -136,161 +91,46 @@ namespace MigToolExtractor
             return count;
         }
 
-        private void initprogressbar(ProgressBar progressBar, int cnt)
+        public void initprogressbar(ProgressBar progressBar, int cnt)
         {
-            progressBar.Minimum = 0;
-            progressBar.Maximum = cnt;
-            progressBar.Value = 0;
+            progressBar.SafeInvoke(d => d.Minimum = 0);
+            progressBar.SafeInvoke(d => d.Maximum = cnt);
+            progressBar.SafeInvoke(d => d.Value = 0);
         }
 
-        private void changeprogressbar(int stepcnt)
+        public void changeprogressbar(ProgressBar progressBar, int stepcnt)
         {
-            progressBar2.Value = progressBar2.Value + stepcnt;
+            if ((progressBar.Value + stepcnt > progressBar.Minimum) && (progressBar.Value + stepcnt <= progressBar.Maximum))
+            {
+                progressBar.SafeInvoke(d => d.Value = progressBar.Value + stepcnt);
+            }
+
         }
 
-        private void ProcessFile(Object sender, ScanEventArgs e)
+        public void ProcessFile(Object sender, ScanEventArgs e)
         {
-            textBox2.Text = e.Name;
-            textBox2.Update();
-
-            changeprogressbar(1);
-
-            textBox3.Text = ((progressBar2.Value / progressBar2.Maximum) * 100) + "% concluído";
-            textBox3.Update();
+            label2.SafeInvoke(d => d.Text = e.Name);
+            changeprogressbar(progressBar2, 1);
+            textBox4.SafeInvoke(d => d.Text = Convert.ToInt32(((Convert.ToDecimal(progressBar2.Value) / Convert.ToDecimal(progressBar2.Maximum)) * 100)).ToString() + "% concluído");
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Zip File";
+            theDialog.Filter = "ZIP files|*.zip";
+            theDialog.InitialDirectory = @"C:\";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.SafeInvoke(d => d.Text = theDialog.FileName);
+            }
+        }
 
-        #region Backup
-        int filecounts = 0;
-
-
-        //Public Sub UnZip(ByVal zipName As String, ByVal FolderDestination As String)
-        //    TextBox1.Text = "Efetuando restore de " & FolderDestination & ". Aguarde!"
-        //    TextBox1.Update()
-        //    Try
-        //        Dim count As Integer = ZipContentsCount(zipName)
-        //        initprogressbar(ProgressBar1, count)
-
-        //        Dim zipevents As FastZipEvents = New FastZipEvents()
-        //        zipevents.ProcessDirectory = New ProcessDirectoryHandler(AddressOf ProcessDirectory)
-        //        zipevents.ProcessFile = New ProcessFileHandler(AddressOf ProcessFile)
-
-        //        Dim objDelegate As FastZip.ConfirmOverwriteDelegate
-        //        objDelegate = AddressOf NotifyClient
-
-        //        Dim fz As FastZip = New FastZip(zipevents)
-        //        fz.CreateEmptyDirectories = True
-        //        fz.ExtractZip(zipName, FolderDestination, "")
-        //    Catch e As Exception
-        //        MsgBox(e.Message)
-        //        Exit Sub
-        //    End Try
-        //    TextBox1.Text = ""
-        //    TextBox1.Update()
-        //    ProgressBar1.Value = 0
-        //End Sub
-
-        //Friend Sub initprogressbar(ByVal ProgressBar As ProgressBar, ByVal cnt As Integer)
-        //    If Not ProgressBar1 Is Nothing Then
-        //        ProgressBar1 = ProgressBar
-        //        ProgressBar1.Minimum = 0
-        //        ProgressBar1.Maximum = cnt
-        //        ProgressBar1.Value = 0
-        //    End If
-        //End Sub
-
-        //Friend Sub changeprogressbar(ByVal stepcnt As Integer)
-        //    If Not ProgressBar1 Is Nothing Then
-        //        'If ProgressBar1.Value < cnt Then
-        //        ProgressBar1.Value = ProgressBar1.Value + stepcnt
-        //        'End If
-        //    End If
-        //End Sub
-
-        //Friend Sub ProcessFile(ByVal sender As Object, ByVal e As ScanEventArgs)
-        //    TextBox2.Text = e.Name
-        //    TextBox2.Update()
-        //    changeprogressbar(1)
-        //    TextBox3.Text = Int((ProgressBar1.Value / ProgressBar1.Maximum) * 100) & "% concluído"
-        //    TextBox3.Update()
-        //End Sub
-
-        //Friend Sub ProcessDirectory(ByVal sender As Object, ByVal e As DirectoryEventArgs)
-        //    If (e.HasMatchingFiles) Then
-        //        'changeprogressbar(1)
-        //    End If
-        //End Sub
-
-        //Friend Sub DirectoryFailure(ByVal sender As Object, ByVal e As ScanFailureEventArgs)
-        //    Dim logFile As String = bkpFolder & "\BACKUPD.log"
-        //    Dim objLogFile As StreamWriter = New StreamWriter(logFile, True)
-        //    objLogFile.WriteLine(Now & " - " & e.Name & " - " & e.Exception.Message)
-        //    objLogFile.Close()
-        //    'changeprogressbar(1)
-        //    e.ContinueRunning = True
-        //End Sub
-
-        //Friend Sub FileFailure(ByVal sender As Object, ByVal e As ScanFailureEventArgs)
-        //    Dim logFile As String = bkpFolder & "\BACKUPD.log"
-        //    Dim objLogFile As StreamWriter = New StreamWriter(logFile, True)
-        //    objLogFile.WriteLine(Now & " - " & e.Name & " - " & e.Exception.Message)
-        //    objLogFile.Close()
-        //    'changeprogressbar(1)
-        //    e.ContinueRunning = True
-        //End Sub
-
-        //Friend Function NotifyClient(ByVal filename As String) As Boolean
-        //    Dim response As MsgBoxResult
-        //    response = MsgBox("Replace the file " + filename, MsgBoxStyle.YesNo, "Replace File")
-        //    'changeprogressbar(1)
-        //    If response = MsgBoxResult.Yes Then
-        //        Return True
-        //    Else
-        //        Return False
-        //    End If
-        //End Function
-
-        //Friend Function ZipContentsCount(ByVal zipfile As String) As Integer
-        //    Dim count As Integer
-        //    Dim strmZipInputStream As ZipInputStream = New ZipInputStream(File.OpenRead(zipfile))
-        //    Dim objEntry As ZipEntry
-
-        //    objEntry = strmZipInputStream.GetNextEntry()
-        //    count = 0
-        //    While IsNothing(objEntry) = False
-        //        count = count + 1
-        //        objEntry = strmZipInputStream.GetNextEntry()
-        //    End While
-        //    strmZipInputStream.Close()
-
-        //    Return count
-        //End Function
-
-        //'Private filecounts As Integer = 0
-        //Friend Function FolderContentsCount(ByVal targetdir As String) As Integer
-        //    Try
-        //        Dim filestruc() As String = Directory.GetFiles(targetdir)
-        //        filecounts = filecounts + filestruc.Length
-
-        //        Dim dirstruc() As String = Directory.GetDirectories(targetdir)
-        //        For Each nextdir As String In dirstruc
-        //            FolderContentsCount(nextdir.ToString)
-        //        Next
-
-        //        FolderContentsCount = filecounts
-        //        Return FolderContentsCount
-        //    Catch ex As Exception
-        //        'MsgBox(ex.Message)
-        //    End Try
-        //End Function
-        #endregion;
-
-
-
-
-
-
-
-
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+            textBox2.SafeInvoke(d => d.Text = fbd.SelectedPath);
+        }
     }
 }
